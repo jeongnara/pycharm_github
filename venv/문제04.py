b8d8364830908213b122467848aaaf86c04e37e2
user <w2016@e-mirim.hs.kr>
2021-05-14 09:51:02 +0900

    문제 4번 정답 2

--- venv/문제04.py
+++ venv/문제04.py
@@ -2,7 +2,7 @@ def solution(arr):
      left = 0
      right = len(arr) - 1
 
-     while left < len(arr)/2:
+     while left < right:
          # arr[left]과 arr[right]의 값을 서로 바꿈
 
          arr[left], arr[right] = arr[right], arr[left]
